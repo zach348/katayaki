e5b3b25fcb3c29cb8196a48746249b431624a207
Zachary Cutler <zachlcutler@gmail.com>
2016-10-17 16:57:21 -0400

    added routes for users and goals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:index, :show, :update, :edit, :destroy]
+  resources :goals, only: [:new, :create, :index, :edit, :update, :show, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
