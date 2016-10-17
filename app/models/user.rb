a617f8a781498b59a18ac4bd8a35a3e8b872a41d
Zachary Cutler <zachlcutler@gmail.com>
2016-10-17 16:22:53 -0400

    generated devise views/added parameter sanitizer in app controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, length: { in: 5..15 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
