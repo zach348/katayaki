a4707508bf337696203b2debd00d5b0aa568e9c6
Zachary Cutler <zachlcutler@gmail.com>
2016-10-18 12:54:24 -0400

    added associations to group and user through affiliation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :goals, through: :aspirations
+  has_many :groups, through: :affiliations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
