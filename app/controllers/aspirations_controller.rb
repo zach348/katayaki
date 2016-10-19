8fa9da23aeb42f82e6dd3a1e8aca9eb6516376f4
Zachary Cutler <zachlcutler@gmail.com>
2016-10-19 15:34:26 -0400

    json endpoint now returning properly formatted data

--- app/controllers/aspirations_controller.rb
+++ app/controllers/aspirations_controller.rb
@@ -4,19 +4,20 @@ class AspirationsController < ApplicationController
   end
 
   def index
-    respond_to do |format|
-      format.json do
-        data = []
-        aspirations = Aspiration.rate_items_for(current_user)
-        aspirations.each do |aspiration|
-          title = Goal.find(aspiration.goal).title
-          description = Goal.find(aspiration).description
-          data.push(
-            { User.find(aspiration.user.full_name) :  }
-          )
+    if current_user
+      respond_to do |format|
+        format.json do
+          data = []
+          aspirations = Aspiration.rate_items_for(current_user, 10)
+          aspirations.each do |aspiration|
+            hashed = Aspiration.to_hash(aspiration)
+            data.push(hashed)
+          end
+        render json: data
         end
-
       end
+    else
+      raise ActionController::RoutingError.new("Not Found")
     end
   end
 end
--- app/models/aspiration.rb
+++ app/models/aspiration.rb
@@ -7,13 +7,20 @@ class Aspiration < ActiveRecord::Base
   validates :user_id, presence: true
   validates :goal_id, presence: true
 
-  def rate_items_for(user, num)
-    user = current_user
+  def self.rate_items_for(user, num)
     aspirations = []
     groups = user.groups.pluck(:id)
     groups.each do |group_id|
       aspirations += self.where(group: group_id)
     end
     aspirations.shuffle.slice(0,10)
+  end
+
+  def self.to_hash(aspiration)
+    {
+      user: User.find(aspiration.user).full_name,
+      title: Goal.find(aspiration.goal).title,
+      description: Goal.find(aspiration.goal).description
+    }
   end
 end
