93ce8fe1da2dd36a72175360993b4cf84ea7133f
Zachary Cutler <zachlcutler@gmail.com>
2016-10-23 12:49:19 -0400

    began implementing search... may switch to ajax

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show, :update, :edit, :destroy]
-  resources :goals, only: [:new, :show]
+  resources :goals, only: [:new, :show, :index]
   resources :aspirations, only: [:new, :index, :create, :show, :destroy]
   resources :static_pages, only: [:index]
   resources :groups, only: [:show]
 
   get 'rate' => 'ratings#rate'
   post 'endorse' => 'ratings#endorse'
+
+  get 'search' => 'goals#search'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
