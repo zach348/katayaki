a46b9fb8678faca613a9edddc9e684ecab76d8ab
Zachary Cutler <zachlcutler@gmail.com>
2016-10-28 10:50:34 -0400

    implemented pop up definitions

--- react/src/components/Katayaki.js
+++ react/src/components/Katayaki.js
@@ -6,41 +6,80 @@ const Katayaki = props => {
   let id = props.info.id;
   let description = props.info.description;
   let user = props.info.user;
+  let details = props.info.details;
   let endorse = props.endorse;
   let img_url = props.info.img_url
   let next = props.next;
   let btnState = props.btnsDisabled;
+  let showDef = props.showDef;
+  let defDisplayed = props.defDisplayed;
 
     if(img_url == 'default_avatar.png'){
-      return (
-        <div className='row'>
-          <div className='katayaki small-9 small-centered columns'>
-            <div className="card">
-              <h3>{goal}</h3>
-              <h5>{user}</h5>
-                <div className='row' key={String(id)}>
-                  <button disabled={btnState} className="btn" onClick={next}>Skip</button>
-                  <button disabled={btnState} className="btn" onClick={endorse}>Endorse</button>
-                </div>
+      if(defDisplayed){
+        return (
+          <div className='row'>
+            <div className='katayaki small-9 small-centered columns'>
+              <div className="card">
+                <h3 onClick={showDef}>{goal}</h3>
+                <h4>({details})</h4>
+                <h5>{user}</h5>
+                  <div className='row' key={String(id)}>
+                    <button disabled={btnState} className="btn" onClick={next}>Skip</button>
+                    <button disabled={btnState} className="btn" onClick={endorse}>Endorse</button>
+                  </div>
+              </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }else{
+        return (
+          <div className='row'>
+            <div className='katayaki small-9 small-centered columns'>
+              <div className="card">
+                <h3 onClick={showDef}>{goal}</h3>
+                <h5>{user}</h5>
+                  <div className='row' key={String(id)}>
+                    <button disabled={btnState} className="btn" onClick={next}>Skip</button>
+                    <button disabled={btnState} className="btn" onClick={endorse}>Endorse</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
     }else{
-      return (
-        <div className='row'>
-          <div className='katayaki small-9 small-centered columns'>
-            <div className="card">
-              <img src={img_url} className='prof-img'></img>
-              <h3>{goal}</h3>
-                <div className='row' key={String(id)}>
-                  <button disabled={btnState} className="kat-button" onClick={next}>Skip</button>
-                  <button disabled={btnState} className="kat-button" onClick={endorse}>Endorse</button>
-                </div>
+      if(defDisplayed){
+        return (
+          <div className='row'>
+            <div className='katayaki small-9 small-centered columns'>
+              <div className="card">
+                <img src={img_url} className='prof-img'></img>
+                <h3 onClick={showDef}>{goal}</h3>
+                <p>({details})</p>
+                  <div className='row' key={String(id)}>
+                    <button disabled={btnState} className="kat-button" onClick={next}>Skip</button>
+                    <button disabled={btnState} className="kat-button" onClick={endorse}>Endorse</button>
+                  </div>
+              </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }else{
+        return (
+          <div className='row'>
+            <div className='katayaki small-9 small-centered columns'>
+              <div className="card">
+                <img src={img_url} className='prof-img'></img>
+                <h3 onClick={showDef}>{goal}</h3>
+                  <div className='row' key={String(id)}>
+                    <button disabled={btnState} className="kat-button" onClick={next}>Skip</button>
+                    <button disabled={btnState} className="kat-button" onClick={endorse}>Endorse</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
     }
 };
 
