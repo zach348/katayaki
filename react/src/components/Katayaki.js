627247ca0468598f4a60d13ccb0a7c0e0676fead
Zachary Cutler <zachlcutler@gmail.com>
2016-10-28 10:59:42 -0400

    adjusted rating view styling

--- react/src/components/Katayaki.js
+++ react/src/components/Katayaki.js
@@ -20,7 +20,7 @@ const Katayaki = props => {
           <div className='row'>
             <div className='katayaki small-9 small-centered columns'>
               <div className="card">
-                <h3 onClick={showDef}>{goal}</h3>
+                <h3 className='goal-title' onClick={showDef}>{goal}</h3>
                 <h4>({details})</h4>
                 <h5>{user}</h5>
                   <div className='row' key={String(id)}>
@@ -36,7 +36,7 @@ const Katayaki = props => {
           <div className='row'>
             <div className='katayaki small-9 small-centered columns'>
               <div className="card">
-                <h3 onClick={showDef}>{goal}</h3>
+                <h3 className='goal-title' onClick={showDef}>{goal}</h3>
                 <h5>{user}</h5>
                   <div className='row' key={String(id)}>
                     <button disabled={btnState} className="btn" onClick={next}>Skip</button>
@@ -54,7 +54,7 @@ const Katayaki = props => {
             <div className='katayaki small-9 small-centered columns'>
               <div className="card">
                 <img src={img_url} className='prof-img'></img>
-                <h3 onClick={showDef}>{goal}</h3>
+                <h3 className='goal-title' onClick={showDef}>{goal}</h3>
                 <p>({details})</p>
                   <div className='row' key={String(id)}>
                     <button disabled={btnState} className="kat-button" onClick={next}>Skip</button>
@@ -70,7 +70,7 @@ const Katayaki = props => {
             <div className='katayaki small-9 small-centered columns'>
               <div className="card">
                 <img src={img_url} className='prof-img'></img>
-                <h3 onClick={showDef}>{goal}</h3>
+                <h3 className='goal-title' onClick={showDef}>{goal}</h3>
                   <div className='row' key={String(id)}>
                     <button disabled={btnState} className="kat-button" onClick={next}>Skip</button>
                     <button disabled={btnState} className="kat-button" onClick={endorse}>Endorse</button>
