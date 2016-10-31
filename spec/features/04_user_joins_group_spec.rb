8bb156e64daacf94e8c30394f36ba3e46193044b
Zachary Cutler <zachlcutler@gmail.com>
2016-10-30 20:51:43 -0400

    added sign up, group, and katayaki specs

---
+++ spec/features/04_user_joins_group_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'groups' do
+  let!(:group) { FactoryGirl.create(:group, name: 'test_circle')}
+  let!(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user joins existing circle' do
+    sign_in(user)
+    click_link 'Circles'
+    select 'test_circle', from: 'group[group_id]'
+    click_button 'Join Circle'
+
+    expect(page).to have_content('You have joined test_circle')
+    expect(user.groups.first.name).to eq('test_circle')
+  end
+
+  scenario 'user creates a new circle' do
+    num_groups = Group.all.length
+
+    sign_in(user)
+    click_link 'Circles'
+    fill_in 'group[name]', with: 'another_circle'
+    click_button 'Form a New Circle'
+
+    expect(page).to have_content('Circle created successfully')
+    expect(Group.all.length).to eq(num_groups + 1)
+    expect(Group.all.last.name).to eq('another_circle')
+  end
+end
