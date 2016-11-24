be629c41b90b2486e19ec39b699e70ad9f05e003
Zachary Cutler <zachlcutler@gmail.com>
2016-11-24 14:16:20 -0500

    added wordnik gem and initializer

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'carrierwave', '>= 1.0.0.beta', '< 2.0'
 gem 'mini_magick'
 gem 'fog'
 gem 'faker'
+gem 'wordnik'
 
 
 group :development, :test do
---
+++ config/initializers/wordnik.rb
@@ -0,0 +1,6 @@
+
+Wordnik.configure do |config|
+  config.api_key = 'a067c2a90562079f350090d80e309cf923694544f0b1ff716'               # required
+  config.response_format = 'json'             # defaults to json, but xml is also supported
+  config.logger = Logger.new('/dev/null')     # defaults to Rails.logger or Logger.new(STDOUT). Set to Logger.new('/dev/null') to disable logging.
+end
