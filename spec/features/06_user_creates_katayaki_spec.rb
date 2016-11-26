2abc2b7b66b6c24cb29b5684deb66d74c2e7126e
Zachary Cutler <zachlcutler@gmail.com>
2016-11-26 14:22:12 -0500

    added spec that hits search/api call

---
+++ spec/features/06_user_creates_katayaki_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'katayakies' do
+  let!(:group) { FactoryGirl.create(:group, name: 'test_circle')}
+  let!(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user creates goal and accepts katayaki' do
+    num_aspirations = user.aspirations.count
+    num_goals = Goal.count
+
+    sign_in(user)
+    click_link 'Circles'
+    select 'test_circle', from: 'group[group_id]'
+    click_button 'Join Circle'
+    click_link 'Aspire'
+    fill_in 'search', with: 'zeal'
+    click_button 'Search'
+
+    expect(page).to have_content('Zeal')
+    expect(Goal.count).to eq(num_goals)
+    expect(Goal.where(title: 'Zeal').empty?).to eq(true)
+
+    click_link 'Zeal'
+    click_button 'Accept Katayaki'
+
+    expect(page).to have_content('Katayaki Accepted')
+    expect(Goal.count).to eq(num_goals + 1)
+    expect(user.aspirations.count).to eq(num_aspirations + 1)
+    expect(Goal.where(title: 'Zeal').first.details).to eq(':eagerness and  interest in pursuit of something : ')
+  end
+
+end
