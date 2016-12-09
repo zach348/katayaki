4f926e98bbe0e3420439fb750cf596ad23d710e3
Zachary Cutler <zachlcutler@gmail.com>
2016-12-09 13:22:09 -0500

    changed default map for visitors

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
   //update location
-  if($('.users').length == 1 || $('.static_pages.index').length == 1) { getLocation(sendPosition); }
+  if($('.users').length == 1 || $('.static_pages.index').length == 1) {
+    getLocation(sendPosition);
+  }
 
   //build map
   if($('.static_pages.index.current_user').length == 1) {
     getCurrentUserPosition(buildMap);
   }else if($('.static_pages.index.visitor').length == 1) {
-    getLocation(buildMap);
+    setTimeout(function(){
+      buildMap({coords: {latitude: 0, longitude: -18.281250}}, 2);
+    }, 100);
   }
 })
