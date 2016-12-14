1a9c05d9ac7efc876ce17d32607213f736284989
Zachary Cutler <zachlcutler@gmail.com>
2016-12-14 10:25:13 -0500

    adjusted footer, borders; fixed redirect on empty search results; changed rendering of footer to path-dependent conditional in app-layout

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -20,7 +20,7 @@ class GoalsController < ApplicationController
     @goals = display_goals.paginate(page: params[:page], per_page: 10)
     if @goals.empty?
       flash[:notice] = 'Try a noun or adjective...'
-      redirect_to goals_path
+      redirect_to goals_path(get_splash: true)
     end
   end
 
