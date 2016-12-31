87ceab21aca387048bf32e66c45a73893b74f1a5
Zachary Cutler <zachlcutler@gmail.com>
2016-12-31 16:56:39 -0500

    added custom domain to actionmailer settings in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'mg.self-app.io',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
