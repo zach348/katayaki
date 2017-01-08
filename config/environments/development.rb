fae038dbd9609ee068b04d698b75ea29924d85aa
Admin <admin@Admins-MBP.lan>
2017-01-08 16:37:10 -0500

    confirmable functionality up and running in dev environment - need to test production and add specs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
