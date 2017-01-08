4458e5dd5f2182c3785db5c3cb333661b2ac7d2e
Zack <zachlcutler@gmail.com>
2017-01-08 18:36:18 -0500

    tried changing mailer settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
   }
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :protocol => 'https', :host => 'self-app.io' }
 end
